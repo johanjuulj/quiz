4909f096f0fc2fbcaa40a80d7aae017f364d1460
Johan <johanjuulj@gmail.com>
2023-03-03 21:01:16 +0100

    fixed bug

--- mondal/script.js
+++ mondal/script.js
@@ -4,21 +4,17 @@ const btnCloseModal = document.querySelector(".close-modal");
 const btnShowModal = document.querySelectorAll(".show-modal");
 
 for (let i = 0; i < btnShowModal.length; i++) {
-  btnShowModal[i].addEventListener("click", showModal);
-}
-
-function showModal() {
-  console.log("button clicked");
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+  btnShowModal[i].addEventListener("click", function () {
+    console.log("button clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
 }
 
 btnCloseModal.addEventListener("click", closeModal);
 
-
-function closeModal(){
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 }
-overlay.addEventListener("click",closeModal);
-};
+overlay.addEventListener("click", closeModal);

42af1c2ddfaafd64c46ac9f8ce3dcbb1c08437b9
Johan <johanjuulj@gmail.com>
2023-03-03 11:08:43 +0100

    set victory color

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 "use strict";
 
-const number = Math.random();
+const number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 
 let message = document.querySelector(".message");
 console.log(message);
 
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 1;
+document.querySelector(".number").textContent = number;
+document.querySelector(".score").textContent = score;
 
 document.querySelector(".check").addEventListener("click", check);
 
@@ -14,12 +15,25 @@ function check() {
   let guess = Number(document.querySelector(".guess").value);
 
   console.log("clicked");
-
+  if (score === 0) {
+    alert("Hallo! You lost please start over and try again!");
+  }
   if (!guess) {
     document.querySelector(".message").textContent = "Please input a guess!";
-  }
-  console.log(number);
-  if (guess === number) {
+  } else if (guess === number) {
     document.querySelector(".message").textContent = "Correct guess!";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "25rem";
+  } else if (guess > number) {
+    document.querySelector(".message").textContent = "Your guess is to high!";
+    score--;
+  } else if (guess < number) {
+    document.querySelector(".message").textContent = "Your guess is to low!";
+    score--;
+  }
+  document.querySelector(".score").textContent = score;
+  if (score === 0) {
+    document.querySelector(".message").textContent =
+      "You lost, please try again!";
   }
 }

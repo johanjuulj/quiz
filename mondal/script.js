bf37d8301ca517b4a6e5a7e159089a14b433f771
Johan <johanjuulj@gmail.com>
2023-03-06 11:18:56 +0100

    added openmodal function

--- mondal/script.js
+++ mondal/script.js
@@ -3,18 +3,20 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnShowModal = document.querySelectorAll(".show-modal");
 
-for (let i = 0; i < btnShowModal.length; i++) {
-  btnShowModal[i].addEventListener("click", function () {
-    console.log("button clicked");
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
+function openModal() {
+  console.log("button clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 }
-
-btnCloseModal.addEventListener("click", closeModal);
-
 function closeModal() {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 }
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener("click", openModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+
 overlay.addEventListener("click", closeModal);
